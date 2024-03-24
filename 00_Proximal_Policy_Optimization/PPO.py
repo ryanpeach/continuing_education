94f417b4588fc1dcea523a1e91911d78cf28f21f
Ryan Peach <rgpeach10@gmail.com>
2024-03-23 22:25:07 -0400

    First notebook

---
+++ 00_Proximal_Policy_Optimization/PPO.py
@@ -0,0 +1,27 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: continuing-education-vJKa4-To-py3.10
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Proximal Policy Optimization (PPO)
+#
+# Paper: https://arxiv.org/abs/1707.06347
+#
+# Other Useful Resources:
+# - https://www.youtube.com/watch?v=HrapVFNBN64
+
+# %%
+import torch
+
+torch.__version__
