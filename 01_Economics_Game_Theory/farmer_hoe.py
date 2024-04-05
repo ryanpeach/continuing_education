7d0a5396d7b3955ed675c57da2dd2c0c6ddf815d
Ryan Peach <rgpeach10@gmail.com>
2024-04-05 10:27:14 -0400

    Create a new section on economics and game theory

---
+++ 01_Economics_Game_Theory/farmer_hoe.py
@@ -0,0 +1,13 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+# ---
+
+import numpy as np
+import sympy as sp
+import networkx as nx
