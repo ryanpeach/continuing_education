475bed9a125e8347ddd90f0bc4506725ae68a06f
Ryan Peach <rgpeach10@gmail.com>
2024-04-14 18:54:08 -0400

    Created a helpful tool for experiment tracking with git

---
+++ continuing_education/lib/experiments/manager.py
@@ -0,0 +1,51 @@
+from pathlib import Path
+import os
+from pprint import pformat
+import warnings
+from git import Repo, GitCommandError
+
+class ExperimentsManager():
+    """
+    A simple git based experiment manager.
+    Put this at the end of your training script, and when it runs, it will commit the changes to the current git branch with the results.
+    """
+    def __init__(self, *, name: str, description: str, primary_metric: str, file: Path | None = None) -> None:
+        self.name = name
+        self.description = description
+        self.file = __file__ if file is None else file
+        self.repo = Repo(search_parent_directories=True)
+        self.primary_metric = primary_metric
+
+        # If we are running in a Jupyter notebook, with jupsync, also commit the .py file 
+        if self.is_jupytext:
+            pair = Path(__file__).with_suffix(".py")
+            if pair.exists():
+                self.files.append(pair)
+
+    @property
+    def is_jupytext(self) -> bool:
+        return self.file.endswith(".ipynb") and Path(self.file).with_suffix(".py").exists()
+    
+    def run_jupytext_sync(self):
+        if self.is_jupytext:
+            os.system(f"jupytext --sync {self.file}")
+
+    def commit(self, metrics: dict[str, float] | None = None):
+        if other_metrics is None:
+            other_metrics = {}
+        self.run_jupytext_sync()
+
+        # Staging files
+        self.repo.git.add(self.file)
+        if self.is_jupytext:
+            self.repo.git.add(Path(self.file).with_suffix('.py'))
+
+        # Check for unstaged changes
+        if self.repo.is_dirty(untracked_files=True):
+            warnings.warn("There are unstaged changes in the repository. Please commit or stage them before running the experiment manager.")
+      
+        # Committing changes
+        commit_message = f"Experiment: {self.name}, {self.primary_metric}: {metrics[self.primary_metric]}"
+        detailed_message = f"{self.description}\n\nResults:\n{pformat(metrics)}"
+        self.repo.git.commit('--allow-empty', '-m', commit_message, '-m', detailed_message)
+    
