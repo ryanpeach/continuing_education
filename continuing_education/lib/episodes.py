a29872be2e5e556326d427825c76d23d8a127076
Ryan Peach <rgpeach10@gmail.com>
2024-04-22 17:05:32 -0400

    Fixed collect_episodes and mypy

--- continuing_education/lib/episodes.py
+++ continuing_education/lib/episodes.py
@@ -1,13 +1,12 @@
 from abc import ABC, abstractmethod
-from argparse import Action
 from dataclasses import dataclass
-from sre_parse import State
 
 from gym import Env
 import torch
 from torch import nn
-
-from continuing_education.lib.types import Done, LogProb, Reward
+from torch import tensor
+from continuing_education.lib.types import DiscreteAction as Action
+from continuing_education.lib.types import State, Done, LogProb, Reward
 
 from typing import Generator
 
@@ -22,10 +21,10 @@ class SARSA:
     next_state: State
     done: Done = Done(False)
     next_action: Action | None = None
-    action_log_prob: LogProb = LogProb(0.0)
+    action_log_prob: LogProb = LogProb(tensor(0.0))
 
 
-class DiscreteActionNetworkInterface(nn.Module, ABC):
+class DiscreteActionPolicyInterface(nn.Module, ABC):
     """This is an abstract class for a feed forward network that outputs discrete actions,
     you must implement the act method for each different type."""
 
@@ -72,24 +71,31 @@ class DiscreteActionNetworkInterface(nn.Module, ABC):
 
 
 def collect_episode(
-    *, env: Env, policy: DiscreteActionNetworkInterface, max_t: int, **policy_kwargs
+    *, env: Env, policy: DiscreteActionPolicyInterface, max_t: int, **policy_kwargs
 ) -> Generator[SARSA, None, None]:
     """A generator that yields SARSA tuples for a single episode."""
     state, _ = env.reset()
     action, action_logprob = policy.act(state, **policy_kwargs)
-    next_state, reward, done, _, _ = env.step(action)
+
     for _ in range(max_t):
-        next_action, next_action_logprob = policy.act(next_state, **policy_kwargs)
+        next_state, reward, done, _, _ = env.step(action)
+        next_action: Action | None = None
+        next_action_logprob = LogProb(tensor(0.0))
+        if not done:
+            next_action, next_action_logprob = policy.act(next_state, **policy_kwargs)
+
         yield SARSA(
             state=State(state),
             action=Action(action),
             reward=Reward(reward),
             next_state=State(next_state),
-            next_action=Action(next_action),
-            done=done,
-            action_log_prob=action_logprob,
+            next_action=Action(next_action) if next_action is not None else None,
+            done=Done(done),
+            action_log_prob=LogProb(action_logprob),
         )
+
         if done:
             break
+
+        assert next_action is not None
         state, action, action_logprob = next_state, next_action, next_action_logprob
-        next_state, reward, done, _, _ = env.step(action)
